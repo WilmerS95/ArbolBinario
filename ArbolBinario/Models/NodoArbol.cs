ba4ca657f2e29d8c166c8d37d798c6238480d084
WilmerS95 <wilmerantoniosinay@gmail.com>
2024-05-17 11:17:02 -0600

    Creacion NodoArbol con sus constructores

---
+++ ArbolBinario/Models/NodoArbol.cs
@@ -0,0 +1,35 @@
+﻿namespace ArbolBinario.Models
+{
+    internal class NodoArbol
+    {
+        public NodoArbol? subArbolIzquierdo { get; set; }
+        public NodoArbol? subArbolDerecho { get; set; }
+        public object? info { get; set; }
+
+        public NodoArbol()
+        {
+            subArbolIzquierdo = null;
+            subArbolDerecho = null;
+            info = null;
+        }
+
+        public NodoArbol(object? _info)
+        {
+            info = _info;
+            subArbolDerecho=null;
+            subArbolIzquierdo=null;
+        }
+
+        public NodoArbol(NodoArbol? _subArbolIzquierdo, NodoArbol? _subArbolDerecho, object? _info)
+        {
+            subArbolIzquierdo = _subArbolIzquierdo;
+            subArbolDerecho = _subArbolDerecho;
+            info = _info;
+        }
+
+        public override string ToString()
+        {
+            return $"/{info}\\";
+        }
+    }
+}
