1c6bccfadaba9c822b295e392d9e0e6ebd041043
WilmerS95 <wilmerantoniosinay@gmail.com>
2024-05-19 20:22:49 -0600

    implementacion grafica arbol

--- ArbolBinario/Program.cs
+++ ArbolBinario/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ArbolBinarioBlazor.Services;
@@ -6,6 +9,14 @@ using ArbolBinario;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
 
 builder.Services.AddScoped<ArbolBinarioService>();
 
